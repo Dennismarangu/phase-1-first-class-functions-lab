77b7da2c7fbfc1fd6837310c2bd958b36c2b3cce
+ <+>
2023-03-15 20:16:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  const drivers =['Bob', 'Alice', 'Charlie', 'Dave'];
+  const result = returnFirstTwoDrivers(drivers);
+  console.log(result);
+
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  const drivers2 =['Bob', 'Alice', 'Charlie', 'Dave'];
+  const result2 = returnLastTwoDrivers(drivers);
+  console.log(result);
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  console.log(selectingDrivers[0](drivers)); 
+
+  console.log(selectingDrivers[1](drivers)); 
+
+  console.log(selectingDrivers[0](drivers));
+
+  console.log(selectingDrivers[1](drivers));
+
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+  console.log(fareDoubler(10));
+  
+  const fareTripler = createFareMultiplier(3);
+  console.log(fareTripler(10));
+
+  const fareQuadrupler = createFareMultiplier(4);
+  console.log(fareQuadrupler(10))
+
+function fareDoubler2(fare) {
+    return createFareMultiplier(2)(fare);
+  }
+  
+  console.log(fareDoubler(10));
+
+
+function doubleFare(fare) {
+  return fare * 2;
+}
+function fareDoubler3(fare) {
+  return doubleFare(fare);
+}
+
+   function tripleFare(fare) {
+    return fare * 3;
+  }
+  function fareTripler2(fare) {
+    return tripleFare(fare);
+  }
+
+  function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+  }
+  
+  function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
